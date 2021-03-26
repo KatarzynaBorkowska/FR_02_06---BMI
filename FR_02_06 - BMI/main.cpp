58a1f8b812fe01f575ccfa3a5dee8a3f5b9008d4
Kasia <71874062+KatarzynaBorkowska@users.noreply.github.com>
2021-03-26 16:46:02 +0100

    Initial Commit

---
+++ FR_02_06 - BMI/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FR_02_06 - BMI
+//
+//  Created by Kasia on 26/03/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
